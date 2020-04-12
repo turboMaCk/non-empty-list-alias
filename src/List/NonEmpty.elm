2caa383becdd54b92c485a9dcb5f4204b510657e
Marek Fajkus <marek.faj@gmail.com>
2020-04-12 20:24:48 +0200

    implement comonadic functions for NonEmptyList (#6)

--- src/List/NonEmpty.elm
+++ src/List/NonEmpty.elm
@@ -7,7 +7,8 @@ type alias NonEmptyList a =
 
 {-| Creates nonempty list with only one element.
 
-    singleton 1 --> (1, [])
+    singleton 1
+    --> ( 1, [] )
 
 -}
 singleton : a -> NonEmptyList a
@@ -17,9 +18,11 @@ singleton h =
 
 {-| Converts List to Maybe NonEmptyList
 
-    fromList [ 1, 2 ] --> Just (1, [2])
+    fromList [ 1, 2 ]
+    --> Just ( 1, [ 2 ] )
 
-    fromList [] --> Nothing
+    fromList []
+    --> Nothing
 
 -}
 fromList : List a -> Maybe (NonEmptyList a)
@@ -47,7 +50,8 @@ fromCons =
 
 {-| Converts NonEmptyList to List
 
-    toList ( 1, [ 2 ] ) --> [1, 2]
+    toList ( 1, [ 2 ] )
+    --> [1, 2]
 
 -}
 toList : NonEmptyList a -> List a
@@ -55,11 +59,26 @@ toList ( h, t ) =
     h :: t
 
 
+{-| Add element to the begining of `NonEmptylist`.
+
+    cons 2 ( 1, [] )
+    --> (2, [ 1 ])
+
+-}
 cons : a -> NonEmptyList a -> NonEmptyList a
 cons a ( h, t ) =
     ( a, h :: t )
 
 
+{-| Remove first element form `NonEmptylist`.
+
+    uncons ( 3, [ 2, 1 ] )
+    -> ( 3, Just ( 2, [ 1 ] ) )
+
+    uncons ( "hello!", [] )
+    -> ( "hello", Nothing )
+
+-}
 uncons : NonEmptyList a -> ( a, Maybe (NonEmptyList a) )
 uncons ( h, t ) =
     ( h, fromList t )
@@ -67,7 +86,8 @@ uncons ( h, t ) =
 
 {-| Returns first element of the nonempty list
 
-    head ( 1, [ 2 ] ) --> 1
+    head ( 1, [ 2 ] )
+    --> 1
 
 -}
 head : NonEmptyList a -> a
@@ -78,7 +98,8 @@ head ( h, _ ) =
 {-| Returns tail of the nonempty list.
 The return type is List and may be empty.
 
-    tail ( 1, [ 2, 3 ] ) --> [2, 3]
+    tail ( 1, [ 2, 3 ] )
+    --> [2, 3]
 
 -}
 tail : NonEmptyList a -> List a
@@ -88,9 +109,11 @@ tail ( _, t ) =
 
 {-| Returns last element of the nonempty list.
 
-    last ( 1, [ 2 ] ) --> 2
+    last ( 1, [ 2 ] )
+    --> 2
 
-    last ( 1, [] ) --> 1
+    last ( 1, [] )
+    --> 1
 
 -}
 last : NonEmptyList a -> a
@@ -113,9 +136,11 @@ lastHelper xs =
 
 {-| Removes first element of the nonempty list.
 
-    dropHead ( 1, [ 2 ] ) --> Just (2, [])
+    dropHead ( 1, [ 2 ] )
+    --> Just (2, [])
 
-    dropHead ( 1, [] ) --> Nothing
+    dropHead ( 1, [] )
+    --> Nothing
 
 -}
 dropHead : NonEmptyList a -> Maybe (NonEmptyList a)
@@ -129,7 +154,11 @@ dropHead ( _, t ) =
 
 {-| Map a function over nonempty list.
 
-    map (\x -> x + 1) ( 1, [ 2, 3 ] ) --> (2, [3, 4])
+    map (\x -> x + 1) ( 1, [ 2, 3 ] )
+    --> ( 2, [3, 4] )
+
+    map String.fromInt ( 1, [ 2 ] )
+    --> ( "1", [ "2" ] )
 
 -}
 map : (a -> b) -> NonEmptyList a -> NonEmptyList b
@@ -190,9 +219,11 @@ reverse ( h, t ) =
 
 {-| Figure out whether a list contains a value.
 
-    member 2 ( 1, [ 2 ] ) --> True
+    member 2 ( 1, [ 2 ] )
+    --> True
 
-    member 3 ( 1, [ 2 ] ) --> False
+    member 3 ( 1, [ 2 ] )
+    --> False
 
 -}
 member : a -> NonEmptyList a -> Bool
@@ -202,9 +233,11 @@ member a ( h, t ) =
 
 {-| Determine if all elements satisfy some test.
 
-    all Char.isUpper ( 'A', [ 'B' ] ) --> True
+    all Char.isUpper ( 'A', [ 'B' ] )
+    --> True
 
-    all Char.isUpper ( 'a', [ 'B' ] ) --> False
+    all Char.isUpper ( 'a', [ 'B' ] )
+    --> False
 
 -}
 all : (a -> Bool) -> NonEmptyList a -> Bool
@@ -214,9 +247,11 @@ all f ( h, t ) =
 
 {-| Determine if any elements satisfy test.
 
-    any Char.isUpper ( 'a', [ 'B' ] ) --> True
+    any Char.isUpper ( 'a', [ 'B' ] )
+    --> True
 
-    any Char.isUpper ( 'a', [ 'b' ] ) --> False
+    any Char.isUpper ( 'a', [ 'b' ] )
+    --> False
 
 -}
 any : (a -> Bool) -> NonEmptyList a -> Bool
@@ -226,7 +261,8 @@ any f ( h, t ) =
 
 {-| Find the maximum element.
 
-    maximum ( 5, [ 3, 1, 2 ] ) --> 5
+    maximum ( 3, [ 3, 5, 2 ] )
+    --> 5
 
 -}
 maximum : NonEmptyList comparable -> comparable
@@ -245,7 +281,8 @@ maximum ( h, t ) =
 
 {-| Find the minimum element.
 
-    minimum ( 5, [ 3, 1, 2 ] ) --> 1
+    minimum ( 3, [ 3, 5, 2 ] )
+    --> 2
 
 -}
 minimum : NonEmptyList comparable -> comparable
@@ -264,7 +301,8 @@ minimum ( h, t ) =
 
 {-| Get the sum of the list elements.
 
-    sum ( 2, [ 2, 2 ] ) --> 6
+    sum ( 2, [ 2, 2 ] )
+    --> 6
 
 -}
 sum : NonEmptyList number -> number
@@ -274,7 +312,8 @@ sum =
 
 {-| Get the product of the list elements.
 
-    product ( 2, [ 2, 2 ] ) --> 8
+    product ( 2, [ 2, 2 ] )
+    --> 8
 
 -}
 product : NonEmptyList number -> number
@@ -284,7 +323,8 @@ product =
 
 {-| Put two lists together.
 
-    append ( 1, [ 2, 3 ] ) ( 4, [ 5 ] ) --> (1, [2, 3, 4, 5])
+    append ( 1, [ 2, 3 ] ) ( 4, [ 5 ] )
+    --> ( 1, [ 2, 3, 4, 5 ] )
 
 -}
 append : NonEmptyList a -> NonEmptyList a -> NonEmptyList a
@@ -317,9 +357,11 @@ concat ( h, t ) =
 
 {-| Map a given function onto a list and flatten the resulting lists.
 
-    concatMap singleton ( 1, [ 2 ] ) -->  (1, [2])
+    concatMap singleton ( 1, [ 2 ] )
+    -->  ( 1, [ 2 ] )
 
-    concatMap (\x -> ( x + 1, [ x + 1 ] )) ( 1, [ 2 ] ) --> (2, [2, 3, 3])
+    concatMap (\x -> ( x + 1, [ x + 1 ] )) ( 1, [ 2 ] )
+    --> ( 2, [ 2, 3, 3 ] )
 
 -}
 concatMap : (a -> NonEmptyList b) -> NonEmptyList a -> NonEmptyList b
@@ -327,13 +369,67 @@ concatMap f =
     concat << map f
 
 
+{-| Create `NonEmptyList` containing sub `NoneEmptyList`s.
+
+This is a more advanced function following [`Comonad`](https://hackage.haskell.org/package/comonad)
+
+    duplicate ( 1, [ 2, 3 ] )
+    --> ( ( 1, [ 2, 3 ] ), [ ( 2, [ 3 ] ), ( 3, [] ) ] )
+
+    duplicate ( "alone", [] )
+    --> ( ( "alone", [] ), [  ] )
+
+-}
+duplicate : NonEmptyList a -> NonEmptyList (NonEmptyList a)
+duplicate ne =
+    ( ne
+    , case fromList <| tail ne of
+        Nothing ->
+            []
+
+        Just sec ->
+            List.reverse <| duplicateHelper [] sec
+    )
+
+
+duplicateHelper : List (NonEmptyList a) -> NonEmptyList a -> List (NonEmptyList a)
+duplicateHelper acc (( _, t ) as ne) =
+    case fromList t of
+        Nothing ->
+            ne :: acc
+
+        Just newNE ->
+            duplicateHelper (ne :: acc) newNE
+
+
+{-| Take sub `NonEmptyList` for each part of `NoneEmptyList` to generate new `NonEmptyList`
+
+This is a more advanced function following [`Comonad`](https://hackage.haskell.org/package/comonad)
+
+    -- for each element sum all allements till the end
+    extend sum ( 1, [ 2, 3 ] )
+    --> ( 6, [ 5, 3 ] )
+
+    -- calculate lenght at each point of NonEmptylist
+    extend length ("foo", [ "bar", "baz", "EOF"] )
+    --> ( 4, [ 3, 2, 1 ])
+
+-}
+extend : (NonEmptyList a -> b) -> NonEmptyList a -> NonEmptyList b
+extend f =
+    map f << duplicate
+
+
 {-| Places the given value between all members of the given list.
 
-    intersperse "and" ( "1", [ "2", "3" ] ) --> ("1", ["and", "2", "and", "3"])
+    intersperse "and" ( "1", [ "2", "3" ] )
+    --> ("1", ["and", "2", "and", "3"])
 
-    intersperse "and" ( "1", [ "2" ] ) --> ("1", ["and", "2"])
+    intersperse "and" ( "1", [ "2" ] )
+    --> ("1", ["and", "2"])
 
-    intersperse "and" ( "1", [] ) --> ("1", [])
+    intersperse "and" ( "1", [] )
+    --> ("1", [])
 
 -}
 intersperse : a -> NonEmptyList a -> NonEmptyList a
@@ -349,11 +445,17 @@ intersperse x ne =
 {-| Combine two lists with a given function
 In case where one of the two lists is longer the extra elements are ignored
 
-    map2 (+) ( 1, [ 2 ] ) ( 1, [ 1 ] ) --> (2, [3])
+    map2 (+) ( 1, [ 2 ] ) ( 1, [ 1 ] )
+    --> (2, [3])
 
-    map2 (+) ( 1, [] ) ( 1, [ 1 ] ) --> (2, [])
+    map2 (+) ( 1, [] ) ( 1, [ 1 ] )
+    --> (2, [])
 
-    map2 (+) ( 1, [ 1 ] ) ( 1, [] ) --> (2, [])
+    map2 (+) ( 1, [ 1 ] ) ( 1, [] )
+    --> (2, [])
+
+    map2 Tuple.pair ( 1, [ 2, 3 ]) ("foo", [ "bar" ])
+    --> ( ( 1, "foo"), [ ( 2, "bar" ) ] )
 
 -}
 map2 : (a -> b -> c) -> NonEmptyList a -> NonEmptyList b -> NonEmptyList c
@@ -363,8 +465,21 @@ map2 f ( h1, t1 ) ( h2, t2 ) =
 
 {-| Map over number of nonempty lists
 
-    map (+) (1, [2]) |> andMap (1, [1])
+    map (+) (1, [2])
+    |> andMap (1, [1])
     --> (2, [3])
+
+    type alias User =
+        { name : String
+        , age : Int
+        , admin : Bool
+        }
+
+    ( User, [ User, User ] )
+    |> andMap ( "Alice", [ "Bob", "Charlie"] )
+    |> andMap ( 30, [ 50, 19 ] )
+    |> andMap ( True, [ False, False ])
+    --> ( User "Alice" 30 True, [ User "Bob" 50 False, User "Charlie" 19 False ] )
 
 -}
 andMap : NonEmptyList a -> NonEmptyList (a -> b) -> NonEmptyList b
@@ -387,7 +502,8 @@ sortHelper f ne =
 
 {-| Sort values from lowest to highest
 
-    sort ( 3, [ 4, 1, 2 ] ) --> (1, [2, 3, 4])
+    sort ( 3, [ 4, 1, 2 ] )
+    --> (1, [2, 3, 4])
 
 -}
 sort : NonEmptyList comparable -> NonEmptyList comparable
@@ -397,7 +513,8 @@ sort =
 
 {-| Sort values by a derived property.
 
-    sortBy String.length ( "333", [ "4444", "1", "22" ] ) --> ("1", ["22", "333", "4444"])
+    sortBy String.length ( "333", [ "4444", "1", "22" ] )
+    --> ("1", ["22", "333", "4444"])
 
 -}
 sortBy : (a -> comparable) -> NonEmptyList a -> NonEmptyList a
@@ -414,9 +531,11 @@ sortWith f =
 
 {-| is the nonempty list exactly one element?
 
-    isSingleton ( 1, [] ) --> True
+    isSingleton ( 1, [] )
+    --> True
 
-    isSingleton ( 1, [ 2 ] ) --> False
+    isSingleton ( 1, [ 2 ] )
+    --> False
 
 -}
 isSingleton : NonEmptyList a -> Bool
