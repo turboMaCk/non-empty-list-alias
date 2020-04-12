c1ad5ded9350e13d836ea0072870f607f0537309
kubaracek <me@jakubracek.net>
2020-04-12 15:10:36 +0200

    Document functions, fix order of append

--- src/List/NonEmpty.elm
+++ src/List/NonEmpty.elm
@@ -174,10 +174,11 @@ product =
     List.product << toList
 
 
+{-| Put two lists together.
 
--- append ("1", ["2, 3"]) ("4", ["5"])
+    append (1, [2, 3]) (4, [5]) --> (1, [2, 3, 4, 5])
 
-
+-}
 append : NonEmptyList a -> NonEmptyList a -> NonEmptyList a
 append ne1 ne2 =
     case toList ne2 of
@@ -185,11 +186,14 @@ append ne1 ne2 =
             ne1
 
         _ ->
-            foldr cons ne1 ne2
-
--- concat (("1", ["2", "3"]), [("4", ["5", "6"]), ("7", ["8"]), ("9", []), ("10", ["11"])])
+            foldr cons ne2 ne1
 
 
+{-| Concatenate a bunch of lists into a single list:
+    concat ((1, [2, 3]), [(4, [5, 6]), (7, [8]), (9, []), (10, [11])])
+    --> (1,[2,3,4,5,6,7,8,9,10,11])
+
+-}
 concat : NonEmptyList (NonEmptyList a) -> NonEmptyList a
 concat ( h, t ) =
     let
@@ -202,24 +206,30 @@ concat ( h, t ) =
     ( hx, tx )
 
 
-
--- concatMap (\x -> x) (("1", ["2", "3"]), [("4", ["5", "6"]), ("7", ["8"]), ("9", []), ("10", ["11"])])
--- concatMap (\x -> (x, [x+1])) (1, [2])
--- concatMap (\x -> (x+1, [x+1])) (1, [2])
+{-| Map a given function onto a list and flatten the resulting lists.
 
 
+    concatMap singleton (1, [2])            -->  (1, [2])
+    concatMap (\x -> (x+1, [x+1])) (1, [2]) --> (2, [2, 3, 3])
+
+
+-}
 concatMap : (a -> NonEmptyList b) -> NonEmptyList a -> NonEmptyList b
 concatMap f =
     concat << map f
 
 
--- intersperse "and" ("1", ["2", "3"])
--- intersperse "and" ("1", ["2"])
--- intersperse "and" ("1", [])
+{-| Places the given value between all members of the given list.
 
+    intersperse "and" ( "1", [ "2", "3" ] ) --> ("1", ["and", "2", "and", "3"])
 
+    intersperse "and" ( "1", [ "2" ] ) --> ("1", ["and", "2"])
+
+    intersperse "and" ( "1", [] ) --> ("1")
+
+-}
 intersperse : a -> NonEmptyList a -> NonEmptyList a
-intersperse x ne=
+intersperse x ne =
     case ne of
         ( _, [] ) ->
             ne
@@ -228,27 +238,35 @@ intersperse x ne=
             ( h, x :: List.intersperse x t )
 
 
+{-| Combine two lists with a given function
+In case where one of the two lists is longer the extra elements are ignored
 
--- map2 (+) (1, [2]) (1, [1])
--- map2 (+) (1, []) (1, [1])
--- map2 (+) (1, [1]) (1, [])
+    map2 (+) ( 1, [ 2 ] ) ( 1, [ 1 ] ) --> (2, [3])
 
+    map2 (+) ( 1, [] ) ( 1, [ 1 ] ) --> (2, [])
 
+    map2 (+) ( 1, [ 1 ] ) ( 1, [] ) --> (2, [])
+
+-}
 map2 : (a -> b -> c) -> NonEmptyList a -> NonEmptyList b -> NonEmptyList c
 map2 f ( h1, t1 ) ( h2, t2 ) =
     ( f h1 h2, List.map2 f t1 t2 )
 
 
+{-| Map over number of nonempty lists
 
--- map (+) (1, [2]) |> andMap (1, [1])
+    map (+) (1, [2]) |> andMap (1, [1])
+    --> (2, [3])
 
-
+-}
 andMap : NonEmptyList a -> NonEmptyList (a -> b) -> NonEmptyList b
 andMap =
     map2 (|>)
 
 
-sortHelper : (List a -> List a) -> (a, List a) -> NonEmptyList a
+{-| unexposed sorting helper
+-}
+sortHelper : (List a -> List a) -> ( a, List a ) -> NonEmptyList a
 sortHelper f ne =
     case f <| toList ne of
         h :: t ->
@@ -259,40 +277,40 @@ sortHelper f ne =
             ne
 
 
+{-| Sort values from lowest to highest
 
--- sort (3, [4, 1, 2])
+    sort ( 3, [ 4, 1, 2 ] ) --> (1, [2, 3, 4])
 
-
+-}
 sort : NonEmptyList comparable -> NonEmptyList comparable
 sort =
     sortHelper List.sort
 
 
+{-| Sort values by a derived property.
 
--- sortBy String.length ("333", ["4444", "1", "22"])
+    sortBy String.length ( "333", [ "4444", "1", "22" ] ) --> ("1", ["22", "333", "4444"])
 
-
+-}
 sortBy : (a -> comparable) -> NonEmptyList a -> NonEmptyList a
 sortBy f =
     sortHelper (List.sortBy f)
 
 
+{-| Sort values with a custom comparison function.
+-}
 sortWith : (a -> a -> Order) -> NonEmptyList a -> NonEmptyList a
 sortWith f =
     sortHelper (List.sortWith f)
 
 
-
--- isSingleton ("1", [])
--- isSingleton ("1", ["2"])
-
 {-| is the nonempty list exactly one element?
 
-    isSingleton (1, []) -> True
-    isSingleton (1, [2]) -> False
+    isSingleton ( 1, [] ) --> True
+
+    isSingleton ( 1, [ 2 ] ) --> False
 
 -}
-
 isSingleton : NonEmptyList a -> Bool
 isSingleton ( _, t ) =
     List.isEmpty t
